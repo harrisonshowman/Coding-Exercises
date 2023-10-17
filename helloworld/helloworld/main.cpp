3cc9611a19e52d41a7f7d626b7dc3aac1dfc01b1
Harrison Showman <harrison@showizala.com>
2023-10-16 17:41:35 -0700

    removing exec file

---
+++ helloworld/helloworld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Harrison Showman on 10/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
