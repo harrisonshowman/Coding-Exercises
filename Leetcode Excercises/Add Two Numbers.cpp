77859a3a2a3e76cb0d40bb0a8945a7d06c3c89ba
Harrison Showman <harrison@showizala.com>
2023-11-20 21:56:44 -0800

    Add first 2 LeetCode excercies

---
+++ Leetcode Excercises/Add Two Numbers.cpp
@@ -0,0 +1,8 @@
+//
+//  Add Two Numbers.cpp
+//  
+//
+//  Created by Harrison Showman on 11/20/23.
+//
+
+#include "Add Two Numbers.hpp"
---
+++ Leetcode Excercises/Add Two Numbers.hpp
@@ -0,0 +1,13 @@
+//
+//  Add Two Numbers.hpp
+//  
+//
+//  Created by Harrison Showman on 11/20/23.
+//
+
+#ifndef Add_Two_Numbers_hpp
+#define Add_Two_Numbers_hpp
+
+#include <stdio.h>
+
+#endif /* Add_Two_Numbers_hpp */
