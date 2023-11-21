1ce27db02e8961426bd258b2161576e988a698f0
Harrison Showman <harrison@showizala.com>
2023-11-20 22:09:31 -0800

    Add Two Sum

---
+++ Leetcode Excercises/Two Sum.cpp
@@ -0,0 +1,23 @@
+/*
+ 
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ You can return the answer in any order.
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
